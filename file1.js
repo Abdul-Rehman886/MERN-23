af1dca30381b6716f58b271cfa5b43bef62e539d
abdulrehman <abdulrehmn886@gmail.com>
2024-10-09 18:12:41 +0500

    msg

--- file1.js
+++ file1.js
@@ -1,8 +1,22 @@
+{
 var a = 10
 b=20
 c=a+b
-
-
-var t = 10
-h=20
-m=t+h
+}
+{
+   var g = 10
+        h=20
+        k=g+h
+        }
+        {
+            var g = 10
+            h=20
+            k=g+h
+            }
+            var ttt=67
+                 
+  idf=uu
+  gos=75
+  husyt=dce
+  fiuheihu=8932
+junf=3908
