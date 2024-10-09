c2239509bca249326682177caf7166dcf32e7c3d
abdulrehman <abdulrehmn886@gmail.com>
2024-10-09 19:34:50 +0500

    Variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+//const
+
+const a=1;
+console.log(a); //1
+{
+    const a=2;
+    console.log(a); //2
+}
+console.log(a); //1
+
+
+
+//let
+
+let b=1;
+console.log(b); //1
+{
+    let b=2;
+    console.log(b); //2
+}
+console.log(b); //1
+
+
+//var
+
+var c=1;
+console.log(c); //1
+{
+    var c=2;
+    console.log(c); //2
+}
+console.log(c); //2
