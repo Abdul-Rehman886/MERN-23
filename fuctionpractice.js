dc67f644abcdf9294f0ba234d0394e22abb89559
abdulrehman <abdulrehmn886@gmail.com>
2024-10-12 19:23:05 +0500

    practiced js

--- fuctionpractice.js
+++
@@ -1,5 +0,0 @@
-const arr=[
-    al=234,
-    giih='ali'
-]
-console.log(arr);
---
+++ functionpractice.js
@@ -0,0 +1,52 @@
+let a=[]
+if (a[0]) {
+    console.log('trruee');
+    
+}
+else{
+    console.log('faldssew');
+    
+}
+
+
+let obj={
+ v:  function func (p1,p2){
+return p1+p2
+    }
+}
+console.log(obj.v(1,2));
+
+name();
+function name(p1=66,p2=99){
+    console.log(p1);
+    console.log(p2);
+    
+};
+
+var name= function(role="admin"){
+    return role;
+    
+    
+};
+
+var ali=name("user");
+console.log(ali);
+console.log(ali);
+
+ const name=(p1=33,p2=77)=>{
+    console.log(p1);
+    console.log(p2);
+    
+} 
+name(88)
+const even=()=>{
+    return "hello"
+}
+const value=even()
+console.log(value);
+
+(function name(p1,p2){
+console.log(p1+p2);
+
+})
+(1,2)
