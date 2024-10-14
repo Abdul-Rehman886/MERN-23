b355e8d7546dfc139a6943d2bf18b533011f333f
abdulrehman <abdulrehmn886@gmail.com>
2024-10-14 18:36:44 +0500

    Made a Multiply Table

--- functionpractice.js
+++ functionpractice.js
@@ -1,144 +1,159 @@
-let a=[]
-if (a[0]) {
-    console.log('trruee');
+// let a=[]
+// if (a[0]) {
+//     console.log('trruee');
 
-}
-else{
-    console.log('faldssew');
+// }
+// else{
+//     console.log('faldssew');
 
-}
+// }
 
-let obj={
- v:  function func (p1,p2){
-return p1+p2
-    }
-}
-console.log(obj.v(1,2));
+// let obj={
+//  v:  function func (p1,p2){
+// return p1+p2
+//     }
+// }
+// console.log(obj.v(1,2));
 
-name();
-function name(p1=66,p2=99){
-    console.log(p1);
-    console.log(p2);
+// name();
+// function name(p1=66,p2=99){
+//     console.log(p1);
+//     console.log(p2);
 
-};
+// };
 
-var name= function(role="admin"){
-    return role;
+// var name= function(role="admin"){
+//     return role;
 
-};
+// };
 
-var ali=name("user");
-console.log(ali);
-console.log(ali);
+// var ali=name("user");
+// console.log(ali);
+// console.log(ali);
 
- const name=(p1=33,p2=77)=>{
-    console.log(p1);
-    console.log(p2);
+//  const name=(p1=33,p2=77)=>{
+//     console.log(p1);
+//     console.log(p2);
 
-}
-name(88)
-const even=()=>{
-    return "hello"
-}
-const value=even()
-console.log(value);
+// }
+// name(88)
+// const even=()=>{
+//     return "hello"
+// }
+// const value=even()
+// console.log(value);
 
-(function name(p1,p2){
-console.log(p1+p2);
+// (function name(p1,p2){
+// console.log(p1+p2);
 
-})
-(1,2)
-const newfile=(p1,p2)=>{
-console.log(p1%p2);
+// })
+// (1,2)
+// const newfile=(p1,p2)=>{
+// console.log(p1%p2);
 
-}
-newfile(2,3)
+// }
+// newfile(2,3)
 
- var a;
-a=66
-console.log(a);
-{
+//  var a;
+// a=66
+// console.log(a);
+// {
 
-    const a=99
-    console.log(a);
-}
-console.log(a);
-const a=10==='10'
-console.log(`${a} is not true`);
+//     const a=99
+//     console.log(a);
+// }
+// console.log(a);
+// const a=10==='10'
+// console.log(`${a} is not true`);
 
-const a=44;
-const b=a.toString()
-console.log(typeof b);
+// const a=44;
+// const b=a.toString()
+// console.log(typeof b);
 
-const a = 1-'4';
-console.log(a);
+// const a = 1-'4';
+// console.log(a);
 
-a=1;
-if (a) {
-    console.log("false");
+// a=1;
+// if (a) {
+//     console.log("false");
 
-}
-else{
-    console.log("true");
+// }
+// else{
+//     console.log("true");
 
-}
+// }
 
-let array={ali:11};
-if (array.ali) {
-    console.log("false");
+// let array={ali:11};
+// if (array.ali) {
+//     console.log("false");
 
-}
-else{
-    console.log("true");
+// }
+// else{
+//     console.log("true");
 
-}
+// }
 
-const a={
-    name(p1,p2){
-console.log(p1+p2);
+// const a={
+//     name(p1,p2){
+// console.log(p1+p2);
 
-    }
-}
-a.name(2,4)
+//     }
+// }
+// a.name(2,4)
 
-function name(p1=44,p2=55){
-    console.log(p1);
-    console.log(p2);
+// function name(p1=44,p2=55){
+//     console.log(p1);
+//     console.log(p2);
     
-        }
-        name(1,3)
+//         }
+//         name(1,3)
 
 
-((p1=44,p2=55)=>{
-    console.log(p1);
-    console.log(p2);
+// ((p1=44,p2=55)=>{
+//     console.log(p1);
+//     console.log(p2);
     
-        })
-       (1,3)
+//         })
+//        (1,3)
 
- const name=(p1=44,p2=55)=>{
-    console.log(p1);
-    console.log(p2);
+//  const name=(p1=44,p2=55)=>{
+//     console.log(p1);
+//     console.log(p2);
     
-        }
-      name(1,3)
+//         }
+//       name(1,3)
 
-const name=(role="admin")=>{
-return role
+// const name=(role="admin")=>{
+// return role
+// }
+// console.log(name("student"));
+
+// const name=()=>{
+// return "hello";
+// }
+// const store=name()
+//     console.log(store);
+
+//     a={store}
+// console.log(a);
+
+
+   
+const multiply=(a,b)=>{
+    console.log(`${a} x ${b} = ${a*b}`);
+    
 }
-console.log(name("student"));
-
-const name=()=>{
-return "hello";
-}
-const store=name()
-    console.log(store);
-
-    a={store}
-console.log(a);
-
-    
-    
+const a=15
+multiply(a,1)
+multiply(a,2)
+multiply(a,3)
+multiply(a,4)
+multiply(a,5)
+multiply(a,6)
+multiply(a,7)
+multiply(a,8)
+multiply(a,9)
+multiply(a,10)
 
 
         
