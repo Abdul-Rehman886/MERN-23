dd870481626de49f4a06ecc396668370b498af80
abdulrehman <abdulrehmn886@gmail.com>
2024-10-15 02:03:07 +0500

    practice

--- functionpractice.js
+++ functionpractice.js
@@ -104,22 +104,21 @@ a.name(2,4)
 function name(p1=44,p2=55){
     console.log(p1);
     console.log(p2);
-    
+
         }
         name(1,3)
-
 
 ((p1=44,p2=55)=>{
     console.log(p1);
     console.log(p2);
-    
+
         })
        (1,3)
 
  const name=(p1=44,p2=55)=>{
     console.log(p1);
     console.log(p2);
-    
+
         }
       name(1,3)
 
@@ -137,11 +136,9 @@ const store=name()
     a={store}
 console.log(a);
 
-
-   
 const multiply=(a,b)=>{
     console.log(`${a} x ${b} = ${a*b}`);
-    
+
 }
 const a=15
 multiply(a,1)
@@ -155,5 +152,133 @@ multiply(a,8)
 multiply(a,9)
 multiply(a,10)
 
+const a=89;
+        if (a>90){
+console.log(a +" is greater than 90");
 
+        }
+        else{
+            console.log(a +" is less than 90");
+
+        }
+
+const a=88;
+        if (a%2==0){
+console.log(a +" is a even number");
+
+        }
+        else{
+            console.log(a +" is odd number");
+
+        }
+
+const a=88;
+        if (typeof a =="number"){
+console.log(a +" is a even number");
+
+        }
+        if (a>0){
+            console.log(a +" is greatr than zero");
+
+                    }
+                    if (!a==0){
+                        console.log(a +" is a even number");
+
+                                }
+        else{
+            console.log(a +" is odd number");
+
+        }
+
+const a=88;
+        if (typeof a =="number"){
+console.log(a +" is a even number");
+
+        }
+       else if (a>0){
+            console.log(a +" is greatr than zero");
+
+                    }
+                  else  if (a==0){
+                        console.log(a +" is a even number");
+
+                                }
+        else{
+            console.log(a +" is odd number");
+
+        }
+
+const a="88";
+        if (typeof a =="string"){
+console.log(a +" is a even number");
+if (!a>0){
+    console.log(a +" is greatr than zero");}
+else{
+    console.log(a +' is not a num');
+
+}
+        }
+
+const a=9;
+if (typeof a=="number") {
+    console.log("greater");
+
+}
+else if (a<10) {
+    console.log("loweeer");
+
+}else {
+    console.log("idkl");
+
+}
+     let  a="99";
+a%2==1 ? console.log("true") : console.log("false")
+
+typeof a=="number" ? b="true" : b="false";
+console.log(b);
+
+a="hello";
+if (typeof a=="number") {
+
+   c="true";
+
+} else {
+
+   c="false";
+
+}
+console.log(c);
+
+let a=89;
+let b = a%2==0 ? 'even' : 'odd'
+console.log(b);
+
+a = 1;
+b = 1;
+if (!(a + b)) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+a=2;
+switch (a) {
+    case 0:
+        console.log("it is 0");
         
+        break;
+        case 1:
+            console.log("it is 1");
+            
+            break;
+            
+            case 2:
+            console.log("it is 2");
+            break;
+            
+    default:
+        
+    console.log("it is not equal");
+
+        break;
+}
