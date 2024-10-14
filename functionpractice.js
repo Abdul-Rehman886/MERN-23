ef460e6ebf2471bd148e48286ab2fe58984ed5bb
abdulrehman <abdulrehmn886@gmail.com>
2024-10-14 16:05:17 +0500

    practice

--- functionpractice.js
+++ functionpractice.js
@@ -1,13 +1,12 @@
 let a=[]
 if (a[0]) {
     console.log('trruee');
-    
+
 }
 else{
     console.log('faldssew');
-    
-}
 
+}
 
 let obj={
  v:  function func (p1,p2){
@@ -20,13 +19,12 @@ name();
 function name(p1=66,p2=99){
     console.log(p1);
     console.log(p2);
-    
+
 };
 
 var name= function(role="admin"){
     return role;
-    
-    
+
 };
 
 var ali=name("user");
@@ -36,8 +34,8 @@ console.log(ali);
  const name=(p1=33,p2=77)=>{
     console.log(p1);
     console.log(p2);
-    
-} 
+
+}
 name(88)
 const even=()=>{
     return "hello"
@@ -55,3 +53,92 @@ console.log(p1%p2);
 
 }
 newfile(2,3)
+
+ var a;
+a=66
+console.log(a);
+{
+
+    const a=99
+    console.log(a);
+}
+console.log(a);
+const a=10==='10'
+console.log(`${a} is not true`);
+
+const a=44;
+const b=a.toString()
+console.log(typeof b);
+
+const a = 1-'4';
+console.log(a);
+
+a=1;
+if (a) {
+    console.log("false");
+
+}
+else{
+    console.log("true");
+
+}
+
+let array={ali:11};
+if (array.ali) {
+    console.log("false");
+
+}
+else{
+    console.log("true");
+
+}
+
+const a={
+    name(p1,p2){
+console.log(p1+p2);
+
+    }
+}
+a.name(2,4)
+
+function name(p1=44,p2=55){
+    console.log(p1);
+    console.log(p2);
+    
+        }
+        name(1,3)
+
+
+((p1=44,p2=55)=>{
+    console.log(p1);
+    console.log(p2);
+    
+        })
+       (1,3)
+
+ const name=(p1=44,p2=55)=>{
+    console.log(p1);
+    console.log(p2);
+    
+        }
+      name(1,3)
+
+const name=(role="admin")=>{
+return role
+}
+console.log(name("student"));
+
+const name=()=>{
+return "hello";
+}
+const store=name()
+    console.log(store);
+
+    a={store}
+console.log(a);
+
+    
+    
+
+
+        
