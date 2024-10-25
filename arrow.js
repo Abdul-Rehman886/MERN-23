e310dabec063621b1d8016e05aff1847d9da78ce
abdulrehman <abdulrehmn886@gmail.com>
2024-10-25 20:23:49 +0500

    msg

--- arrow.js
+++ arrow.js
@@ -1,76 +1,125 @@
-const a=2;
-const promise1= new Promise ((res,rej)=>{
-if (a%2==0) {
+// const a=2;
+// const promise1= new Promise ((res,rej)=>{
+// if (a%2==0) {
 
-    setTimeout(() => {
-        res (new Date())
-    }, 3000);
-}
-else{
-rej("odd")
-}
-})
+//     setTimeout(() => {
+//         res (new Date())
+//     }, 3000);
+// }
+// else{
+// rej("odd")
+// }
+// })
 
-const promise2= new Promise ((res,rej)=>{
-if (a%2==0) {
-    res (new Date())
+// const promise2= new Promise ((res,rej)=>{
+// if (a%2==0) {
+//     res (new Date())
 
-}
-else{
-rej("false")
-}
-})
+// }
+// else{
+// rej("false")
+// }
+// })
 
-promise1.then((res)=>{
-console.log(res);
-}).catch((rej)=>{
-    console.log(rej);
-})
+// promise1.then((res)=>{
+// console.log(res);
+// }).catch((rej)=>{
+//     console.log(rej);
+// })
 
-promise2.then((res)=>{
-    console.log(res);
-    }).catch((rej)=>{
-        console.log(rej);
-    })
+// promise2.then((res)=>{
+//     console.log(res);
+//     }).catch((rej)=>{
+//         console.log(rej);
+//     })
 
-const funct= async()=>{
+// const funct= async()=>{
 
-   await promise1.then((res)=>{
-        console.log(res);
-        }).catch((rej)=>{
-            console.log(rej);
-        })
+//    await promise1.then((res)=>{
+//         console.log(res);
+//         }).catch((rej)=>{
+//             console.log(rej);
+//         })
 
-     await   promise2.then((res)=>{
-            console.log(res);
-            }).catch((rej)=>{
-                console.log(rej);
-            })
+//      await   promise2.then((res)=>{
+//             console.log(res);
+//             }).catch((rej)=>{
+//                 console.log(rej);
+//             })
 
-}
-funct()
+// }
+// funct()
+
+// const promise1 = new Promise((res) => {
+//     res(new Date());
+// });
+
+// let promise2 = new Promise((res) => {
+//    setTimeout(() => {
+//         res(new Date());
+//     }, 1000);
+// });
+
+
+// // const funct = async () => {
+//    promise1.then((res) => {
+//         console.log(res);
+//     });
+
+// const func=()=>{
+
+
+// while (promise2  ) {
+
+//     promise2.then((res) => {
+//         console.log(res);
+//         promise2++
+//     });
+// }
+// }
+// func()
+
+
+//     // funct();
+// // };
+
+// // funct();
+
+
+
+
+
+
+// async (newTime)=>{
+// await newTime(()=>{
+// while (1) {
+//    newTime()
+// }
+// })
+
+// }
+// newTime()
+
+
+
+
 
 const promise1 = new Promise((res) => {
-    res(new Date());
-});
-
-const promise2 = new Promise((res) => {
-   setTimeout(() => {
+  setTimeout(() => {
         res(new Date());
     }, 1000);
-});
+})
 
-
-const funct = async () => {
-    await promise1.then((res) => {
+const main = async () => {
+    promise1.then((res) => {
         console.log(res);
+
     });
 
-    await promise2.then((res) => {
-        console.log(res);
-    });
 
-    funct();
-};
+}
 
-funct();
+while (main) {
+        
+    main()
+}
